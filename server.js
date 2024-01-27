f537d7c5f7bacac552ce4d86c97e021a17efe2fa
TeddyG <teddygirma20@gmail.com>
2024-01-27 20:35:30 +0300

    express-router

--- server.js
+++ server.js
@@ -39,52 +39,17 @@ app.use(express.json());
 
 // serve static files
 app.use(express.static(path.join(__dirname, "/public")));
+app.use('/subdir',express.static(path.join(__dirname, "/public")));
 
-// To serve  a file
-app.get("^/$|/index(.html)?", (req, res) => {
-  // res.sendFile('./views/index.html', { root: __dirname });
-  res.sendFile(path.join(__dirname, "views", "index.html"));
-});
+// routes
+app.use('/', require('./routes/root'))
+app.use('/subdir', require('./routes/subdir'));
+app.use('/employees', require('./routes/api/employees'));
 
-app.get("/new-page(.html)?", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "new-page.html"));
-});
 
-// redirect
-app.get("/old-page(.html)?", (req, res) => {
-  res.redirect(301, "/new-page.html");
-});
 
-// route handlers
-app.get(
-  "/hello(.html)?",
-  (req, res, next) => {
-    console.log("attempted to load hello.html");
-    next();
-  },
-  (req, res) => {
-    res.send("Hello World!");
-  }
-);
 
-// chaining route handlers
-const one = (req, res, next) => {
-  console.log("one");
-  next();
-};
 
-const two = (req, res, next) => {
-  console.log("two");
-  next();
-};
-
-const three = (req, res, next) => {
-  console.log("three");
-  res.send("Finished");
-  next();
-};
-
-app.get("/chain(.html)?", [one, two, three]);
 
 app.use(errorHandler);
 
